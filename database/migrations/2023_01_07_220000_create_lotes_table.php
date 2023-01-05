a7598ff7155754f8b19760c2090caa867f2fdef0
Everton Soares Silva <everton_dev@hotmail.com>
2023-01-04 23:39:49 -0300

    Versão 0.4
    Todas as migrations foram feitas.

--- database/migrations/2023_01_07_220000_create_lotes_table.php
+++ database/migrations/2023_01_07_220000_create_lotes_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('registros', function (Blueprint $table) {
+        Schema::create('lotes', function (Blueprint $table) {
             $table->id();
-            $table->foreign('imunizado_id')->references('id')->on('users');
-            $table->foreign('profissional_id')->references('cns')->on('users');
-            $table->foreign('lote_id')->references('id')->on('lotes');
-            $table->date('data_vacinacao');
-            $table->string('CNES');
+            $table->unsignedBigInteger('vacina_id');
+            $table->foreign('vacina_id')->references('id')->on('vacinas');
+            $table->string('codigo')->unique();
+            $table->string('tecnologia');
+            $table->bigInteger('quantidade');
+            $table->date('data_vencimento');
+            $table->string('status')->default('ok');
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('registros');
+        Schema::dropIfExists('lotes');
     }
 };
--- database/migrations/2023_01_09_132348_create_enderecos_table.php
+++ database/migrations/2023_01_09_132348_create_enderecos_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('enderecos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('cep_id');
             $table->foreign('cep_id')->references('id')->on('ceps')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('numero');
             $table->string('complemento');
