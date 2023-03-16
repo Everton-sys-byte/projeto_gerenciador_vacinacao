ff75889871b85b2b9ba74204aa1a0cc98913556e
Everton Soares Silva <everton_dev@hotmail.com>
2023-03-16 12:15:35 -0300

    Versão 0.25
    mudei o overflow.

--- resources/views/components/form/default-form.blade.php
+++ resources/views/components/form/default-form.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ $formAction }}" method="{{ $formMethod }}" id="{{ $formId }}"
     @if ($formEncType) enctype="multipart/form-data" @endif
     {{ $attributes->merge([
-        'class' => 'bg-white rounded shadow-lg d-flex flex-column justify-content-center gap-3',
+        'class' => 'bg-light rounded shadow-lg d-flex flex-column justify-content-center gap-3',
     ]) }}>
     @csrf
     {{ $slot }}
